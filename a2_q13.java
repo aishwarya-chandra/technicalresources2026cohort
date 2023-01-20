2babbae05b799fdc28c9351e935c69f8c7e52838
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2023-01-20 23:43:34 +0530

    Submitting Q3 of A2

---
+++ a2_q13.java
@@ -0,0 +1,15 @@
+public class a2_q13 {
+    public static void main(String[] args) {
+        int n = 6;
+        for (int row = 0; row < n; row++) {
+            int i = 1;
+            for (int col = 0; col <= row; col++) {
+                System.out.print(i + " ");
+                int io = i * (row - col) / (col + 1);
+                i = io;
+            }
+            System.out.println();
+        }
+    }
+
+}
