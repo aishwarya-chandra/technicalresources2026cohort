f1cf5b1a765d1b35de4163459ee69d492ef7e969
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2023-01-20 23:33:20 +0530

    Submitting Q3 of A3

---
+++ a2_q03.java
@@ -0,0 +1,16 @@
+public class a2_q3 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            for (int spaces = 1; spaces <= n - row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
