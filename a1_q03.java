ca6f429697cb2ae54b2a61e878494b3928834ae5
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2023-01-20 23:14:07 +0530

    Submitting Q3 of A1

---
+++ a1_q03.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//if is a prime number
+public class a1_q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        System.out.print(num);
+        if (num == 1) {
+            System.out.println(" is not a prime number");
+            System.exit(0);
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.print(" is not a prime number");
+                System.exit(0);
+            }
+        }
+        System.out.println(" is a prime number");
+    }
+}
