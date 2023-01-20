147763ea51b0ab2c6c2f69a82fd2915307d3eb10
Aishwarya Chandra <122461230+aishwarya-chandra@users.noreply.github.com>
2023-01-20 23:38:38 +0530

    Submitting Q7 of A2

---
+++ a2_q07.java
@@ -0,0 +1,14 @@
+public class a2_q7 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row == col)
+                    System.out.print("* ");
+                else
+                    System.out.print("   ");
+            }
+            System.out.println();
+        }
+    }
+}
